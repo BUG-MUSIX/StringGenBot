9d26df05aed0160ed8f66c6357510e260103efe3
𝐒 ᴏ ʜ ᴇ ʟ ..!! <143880963+SH251204@users.noreply.github.com>
2024-05-08 12:06:27 +0000

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -7,7 +7,8 @@ from StringGen.utils import add_served_user, keyboard
 
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
-    await message.reply_text(
+    await message.reply_photo(
+        photo="https://graph.org/file/0c425a271a2caae5d62dc.jpg",
         text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
