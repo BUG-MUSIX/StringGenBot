ed751b39d928e2182b28929139bf85e3981bb94f
𝐒 ᴏ ʜ ᴇ ʟ ..!! <143880963+SH251204@users.noreply.github.com>
2024-05-12 10:08:36 +0000

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -10,6 +10,5 @@ async def f_start(_, message: Message):
         photo="https://graph.org/file/0c425a271a2caae5d62dc.jpg",
        caption=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
         reply_markup=keyboard,
-        disable_web_page_preview=True,
     )
     await add_served_user(message.from_user.id)
