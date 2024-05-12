d8b599a8b1ad6836e3088b04d81e3204fc6ff70f
𝐒 ᴏ ʜ ᴇ ʟ ..!! <143880963+SH251204@users.noreply.github.com>
2024-05-12 09:54:37 +0000

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -4,13 +4,12 @@ from pyrogram.types import Message
 from StringGen import Anony
 from StringGen.utils import add_served_user, keyboard
 
-START_IMG="https://graph.org/file/0c425a271a2caae5d62dc.jpg"
 
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
-    await message.reply_photo(
-        photo=START_IMG,
-       caption=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+    await message.reply_text(
+        photo="https://graph.org/file/0c425a271a2caae5d62dc.jpg",
+       text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
