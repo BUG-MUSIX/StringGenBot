719032efad05ef0f3a244670b100564d0dd26429
𝐒 ᴏ ʜ ᴇ ʟ ..!! <143880963+SH251204@users.noreply.github.com>
2024-05-12 10:03:51 +0000

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -4,12 +4,11 @@ from pyrogram.types import Message
 from StringGen import Anony
 from StringGen.utils import add_served_user, keyboard
 
-start_txt=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ."
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_photo(
         photo="https://graph.org/file/0c425a271a2caae5d62dc.jpg",
-       caption=start_txt,
+       caption=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
