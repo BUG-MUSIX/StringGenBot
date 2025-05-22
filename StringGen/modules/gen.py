43c9ceacfb6da97117318d68949f39c47703a232
𝐒 ᴏ ʜ ᴇ ʟ ..!! <143880963+BUG-MUSIX@users.noreply.github.com>
2025-05-22 13:08:22 +0530

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@xD_Feelings"))
+            await client(JoinChannelRequest("SpicyxNetwork"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
