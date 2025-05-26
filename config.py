c80ab58f38ebb14d8ece3265ed50019c7c26dafb
𝐒 ᴏ ʜ ᴇ ʟ ..!! <143880963+BUG-MUSIX@users.noreply.github.com>
2025-05-26 21:44:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "23803580"))
+API_HASH = getenv("API_HASH", "7d91da02949db09dc81df55532c93863")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN","7836141612:AAGyJrUcnbj2Qp8mWGQwUkYneZnqiSMcDgU")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://spicystring:Nothing0000@string.rsr5s28.mongodb.net/?retryWrites=true&w=majority&appName=String")
 
 OWNER_ID = int(getenv("OWNER_ID", 7381712992))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SpicyxNetwork")
